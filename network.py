ed33abb991d9e7b8547e338e5f4fac61ec307090
Vincent <vincent.gubitosi@gmail.com>
2015-02-19 21:52:09 -0500

    fixed spacing

--- network.py
+++ network.py
@@ -12,21 +12,21 @@ class Network():
         g = nx.Graph()
         f = open(filename)
         for line in f:
-    	   l = line.split
-    	   if not(l[0] in nodes):
-    		  g.add_node(l[0])
-    	   if not(l[1] in nodes):
-    		  g.add_node(l[1])
-    	   g.add_edge(l[0], l[1])
+            l = line.split
+            if not(l[0] in nodes):
+                g.add_node(l[0])
+            if not(l[1] in nodes):
+                g.add_node(l[1])
+            g.add_edge(l[0], l[1])
         self.graph = g
 
     def createNodes(g):
-	    nodes = dict()
-	    for n in g:
-		    node = Node(n)
-		    for n2 in g.neighbors(n):
-			    node.addNeighbor(n2)
-		    nodes.update(node, n)
+        nodes = dict()
+        for n in g:
+            node = Node(n)
+            for n2 in g.neighbors(n):
+                node.addNeighbor(n2)
+            nodes.update(node, n)
         self.theNodes = nodes
 
     def getGraph():
