a2e70962e0422bc0465821deddac178fceed0afd
Vincent <vincent.gubitosi@gmail.com>
2015-02-19 20:43:03 -0500

    create the network. Still need to update createNodes(g) to add neighbors as nodes instead of ids

---
+++ network.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import sys
+
+class network: 
+
+    def __init__(self):
+        self.graph = nx.Graph()
+        self.theNodes = dict()
+
+    def createNetwork():
+        g = nx.Graph()
+        f = open(sys.argv[1], 'r')
+        for line in f:
+    	   l = line.split
+    	   if not(l[0] in nodes):
+    		  g.add_node(l[0])
+    	   if not(l[1] in nodes):
+    		  g.add_node(l[1])
+    	   g.add_edge(l[0], l[1])
+        self.graph = g
+
+    def createNodes(g):
+	   nodes = dict()
+	   for n in g:
+		  node = Node(n)
+		  for n2 in g.neighbors(n):
+			 node.addNeighbor(n2)
+		  nodes.update(node, n)
+        self.theNodes = nodes
+
+    def getGraph():
+        return self.graph
+
+    def getNodes():
+        return self.theNodes
