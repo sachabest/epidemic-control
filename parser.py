db2372c861748afa26fceef983d9793635fd15c2
Corey Loman <loman@seas.upenn.edu>
2015-02-20 23:51:21 -0500

    added own implementation of BFS to parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import networkx as nx 
+import Queue
 
 def networkParser():
 	G = nx.read_edgelist('tenmill_edges.txt', nodetype=int, create_using=nx.Graph())
@@ -9,7 +10,8 @@ def networkParser():
 	for g in graph_list:
 		i+=1
 		source = nx.nodes(g)[0]
-		edges = nx.bfs_edges(g, source)
+		#edges = nx.bfs_edges(g, source)
+		edges = BFS_edges(g, source)
 		for e1,e2 in edges:
 			print (str(e1) + ' ' + str(e2))
 			subgraph.add_edge(e1, e2)
@@ -21,5 +23,24 @@ def networkParser():
 	print ('Number of Nodes in Subgraph: ' + str(nx.number_of_nodes(subgraph)))
 	print ('Number of Connected Components Used: ' + str(i))
 	print ('Number of Connected Components in Subgraph: ' + str(nx.number_connected_components(subgraph)))
+
+
+def BFS_edges(graph, source):
+	'''takes in a graph and a source node, of type Node
+	returns a list of BFS edges'''
+	q = Queue.Queue()
+	parent_node = {}
+	visited = set()
+	output = []
+	q.put(source)
+	visited.add(source)
+	while not q.empty():
+		curr = q.get()
+		for neighbor in curr.getNeighbors():
+			if not (neighbor in visited):
+				q.put(neighbor)
+				visited.add(neighbor)
+				output.append(curr, neighbor)
+	return output
 
 networkParser()
