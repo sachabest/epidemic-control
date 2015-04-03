8f42391871561157f102ac82da82cfec52cc4279
Vincent <vincent.gubitosi@gmail.com>
2015-04-03 00:13:03 -0400

    Updated seedingStrategies.py

--- seedingStrategies.py
+++ seedingStrategies.py
@@ -8,15 +8,15 @@ class SeedingStrategy:
 	def __init__(self, net, numberNodes, nodes):
 		self.net = net
 		self.numberNodes = numberNodes
-		self.nodes = nodes.values()
+		self.nodes = nodes.getNodes()
 
 	def largestDegrees(self):
 		sortedListByDegrees = sorted(self.net.degree_iter(), key = itemgetter(1), reverse = True)
 		i = self.numberNodes
 		while (i > 0):
 			for node in self.nodes:
-				if (node.getId() == sortedListByDegrees[0][0]):
-					success = node.setState = 'INFECTED'
+				if (self.nodes[node].getId() == sortedListByDegrees[0][0]):
+					success = self.nodes[node].setState('INFECTED')
 					if success:
 						sortedListByDegrees.pop(0)
 						break
@@ -24,14 +24,14 @@ class SeedingStrategy:
 		return self.nodes
 
 	def eigenvectorCentrality(self):
-		sortedListByEigenvectorCentrality = sorted(nx.eigenvector_centrality(self.net), key = itemgetter(1), reverse = True)
+		sortedListByEigenvectorCentrality = sorted(nx.eigenvector_centrality(self.net, max_iter = 10000, tol = .1), key = itemgetter(1), reverse = True)
 		i = self.numberNodes
 		while (i > 0):
 			for node in self.nodes:
-				if (node.getId() == sortedListByEigenvectorCentrality[0][0]):
-					success = node.setState = 'INFECTED'
+				if (self.nodes[node].getId() == sortedListByEigenvectorCentrality[0][0]):
+					success = self.nodes[node].setState = 'INFECTED'
 					if success:
-						sortedListByDegrees.pop(0)
+						sortedListByEigenvectorCentrality.pop(0)
 						break
 			i -= 1
 		return self.nodes
