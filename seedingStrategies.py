d5ec30836a3be405a2348f756fb9fa87c6c51064
Corey Loman <loman@seas.upenn.edu>
2015-04-03 00:26:09 -0400

    stuff

--- seedingStrategies.py
+++ seedingStrategies.py
@@ -5,6 +5,10 @@ from operator import itemgetter
 
 class SeedingStrategy:
 
+
+	# self.net = net.getGraph() -- networkX
+	# self.numberNodes = number of seed nodes
+	# self.nodes = net.getNodes() -- our own node class
 	def __init__(self, net, numberNodes, nodes):
 		self.net = net
 		self.numberNodes = numberNodes
@@ -16,7 +20,7 @@ class SeedingStrategy:
 		while (i > 0):
 			for node in self.nodes:
 				if (node.getId() == sortedListByDegrees[0][0]):
-					success = node.setState = 'INFECTED'
+					success = node.setState('INFECTED')
 					if success:
 						sortedListByDegrees.pop(0)
 						break
@@ -29,7 +33,7 @@ class SeedingStrategy:
 		while (i > 0):
 			for node in self.nodes:
 				if (node.getId() == sortedListByEigenvectorCentrality[0][0]):
-					success = node.setState = 'INFECTED'
+					success = node.setState('INFECTED')
 					if success:
 						sortedListByDegrees.pop(0)
 						break
