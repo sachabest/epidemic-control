44ebbb25de54999df7efe737a257cdb1c9490e4a
Sacha Best <sachabest@outlook.com>
2015-04-16 11:08:21 -0400

    Adding basic node app

--- datavis/main.js
+++ datavis/main.js
@@ -1,3 +1,5 @@
 var container = Documet.getElementById('container');
 
-var globe = new DAT.Globe( container );
+var gephiJSON = loadJSON("gephi.json");
+var data = { gephi: gephiJSON };
+var network = new vis.Network(container, data);
