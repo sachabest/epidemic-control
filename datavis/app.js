f77ab477e843ad20f091d006c7821c1d64a46f5b
Sacha Best <sachabest@outlook.com>
2015-04-16 11:23:51 -0400

    SendFile routes

--- datavis/app.js
+++ datavis/app.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
 	res.render("index");
 });
 
+app.get('main.js', function (req, res) {
+	res.sendFile('main.js');
+});
+
+app.get('vis.js', function (req, res0 ){
+	res.sendFile('node_modules/vis/dist/vis.min.js');
+});
+
+app.get('vis.css', function (req, res0 ){
+	res.sendFile('node_modules/vis/dist/vis.min.css');
+});
+
 var server = app.listen(80, function () {
 
 	var host = server.address().address;
