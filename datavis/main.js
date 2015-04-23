0726f928b779886f229485e76976ded80e722275
Sacha Best <sachabest@outlook.com>
2015-04-23 16:41:34 -0400

    Visualization ready

--- datavis/main.js
+++ datavis/main.js
@@ -18,24 +18,31 @@ $(document).ready(function() {
 			}
 			s.refresh();
 		});
-
+		var playing = false;
 		var i = 1;                  	//  set your counter to 1
 		function play () {        	//  create a loop function
 	  		setTimeout(function () {    //  call a 3s setTimeout when the loop is called
 				$('#timestep').val(i);
 				$('#timestep').change();
-				i++;                    //  increment the counter
-	      		if (i < 1000) {           //  if the counter < 10, call the loop function
-	         		play();           //  ..  again which will trigger another 
-	      		}                       //  ..  setTimeout()
-	   		}, 200)
+				if (playing) {
+					i++;                    //  increment the counter
+		      		if (i < 1000) {           //  if the counter < 10, call the loop function
+		         		play();           //  ..  again which will trigger another 
+		      		}
+		      	}
+	   		}, 50)
 		} 
-
 		$('#play').click(function () {
-			console.log('click');
-			i = 1;
-			play();
-			$('#play').text('Playing');
+			if (!playing) {
+				playing = true;
+				play();
+				$('#play').text('Playing (click to stop)');
+				$('#play').parent().addClass('active');
+			}
+			else {
+				playing = false;
+				$('#play').text('Play');
+			}
 		});
 	});
 	var xmlhttp = new XMLHttpRequest();
@@ -44,6 +51,7 @@ $(document).ready(function() {
 	        timesteps = JSON.parse(xmlhttp.responseText);
 	        console.log('json back');
 			$('#play').prop('disabled', false);
+			$('#play > span').text("Play");
 			$('#timestep').change();
 
 	   	}	
@@ -52,6 +60,3 @@ $(document).ready(function() {
 	xmlhttp.send();
 	
 });
-
-
-
